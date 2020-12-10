ecac70e68eb291e4ac9d93f828e48d3c60b9070d
JoaquinHN <dannycantarero3@gmail.com>
2020-12-09 21:22:38 -0600

    Carousel

--- frontend-website-yourcourses_/src/components/public/Home.js
+++ frontend-website-yourcourses_/src/components/public/Home.js
@@ -1,9 +1,11 @@
 import Navbar from "../common/Navbar";
+import Carousel from '../common/Carousel'
 
 function Home() {
   return (
     <div>
       <Navbar />
+      <Carousel/>
     </div>
   );
 }
