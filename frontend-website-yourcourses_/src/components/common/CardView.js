17e4fe1ac730d016adf2f7f6f5ddef4bb146410b
MarioArgueta <51066024+MarioArgueta@users.noreply.github.com>
2020-12-13 22:38:19 -0600

    Seccion de diseño para el HOME

--- frontend-website-yourcourses_/src/components/common/CardView.js
+++ frontend-website-yourcourses_/src/components/common/CardView.js
@@ -4,19 +4,21 @@ import img from '../public/login.svg'
 
 const CardView = () => {
     return(
+        <div style={{paddingTop : "2%"}}>
         <CardGroup className="container">
-            <Card style={{ width: '18rem', padding: '100px 100px 100px 100px' }}>
+            <Card style={{ width: '18rem', padding: '10px 10px 10px 10px' }}>
             <Card.Img variant="top" src={img} />
             <Card.Body>
                 <Card.Title>Curso de Node.js </Card.Title>
                 <Card.Text>
                     Aprende programacion web, del lado del servidor con Node.js
                 </Card.Text>
-                <Button style={{ width: '100%', background: "#ff8a50", border: "#ff8a50" }}>Mas información...</Button>
+                <Button style={{ width: '100%', background: "#ff8a50", border: "#ff8a50" }}>MAS INFORMACIÓN</Button>
+                <Button style={{ width: '100%', background: "#ff8a50", border: "#ff8a50" }}>SUSCRIBETE</Button>
             </Card.Body>
             </Card>
         </CardGroup>
-        
+        </div>
     );
 }
 export default CardView;
--- frontend-website-yourcourses_/src/components/public/Home.js
+++ frontend-website-yourcourses_/src/components/public/Home.js
@@ -2,12 +2,14 @@ import Navbar from "../common/Navbar";
 import Carousel from '../common/Carousel';
 import Footer from "../common/Footer";
 import CardView from '../common/CardView';
+import Section from '../common/Section'
 
 function Home() {
   return (
     <div>
       <Navbar />
       <Carousel/>
+      <Section/>
       <CardView/>
       <Footer/>
     </div>
